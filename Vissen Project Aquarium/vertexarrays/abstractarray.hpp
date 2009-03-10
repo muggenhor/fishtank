846bca60487ee528d3fb7fbbe928039ee2a64762
Giel <Giel@bb1b31de-baa9-489e-af1c-03370f2903b8>
2009-03-10 15:38:19 +0000

    Add class AbstractArray which will represent different types of OpenGL Vertex Arrays E.g. Vertex Arrays themselves, Texture Coordinate Arrays, Normal Arrays and Vertex Colour Arrays.
    git-svn-id: https://svn.il.fontys.nl/usersvn/fishtank/svn/trunk@160 bb1b31de-baa9-489e-af1c-03370f2903b8

---
+++ Vissen Project Aquarium/vertexarrays/abstractarray.hpp
@@ -0,0 +1,59 @@
+#ifndef __INCLUDED_ABSTRACTARRAY_HPP__
+#define __INCLUDED_ABSTRACTARRAY_HPP__
+
+#include <vector>
+#include <eigen/vector.h>
+
+template <typename CoordType, std::size_t CoordinateCount>
+class AbstractArray
+{
+    public:
+        typedef Eigen::Vector<CoordType, CoordinateCount> value_type;
+
+        /** Virtual destructor to make sure that all subclasses have a virtual
+         *  destructor as well.
+         */
+        virtual ~AbstractArray() {}
+
+        /** Passes all of this AbstractArray's data to the OpenGL API.
+         */
+        virtual void draw() const = 0;
+
+        /** @return the amount of elements in this AbstractArray
+         */
+        std::size_t size() const
+        {
+            return _data.size();
+        }
+
+        /** @return the element at the given index
+         */
+        const value_type& operator[](std::size_t index) const
+        {
+            return _data[index];
+        }
+
+        /** Appends the given element to the end of this AbstractArray.
+         *  @return the index given to this new element.
+         */
+        std::size_t push_back(const value_type& element)
+        {
+            _data.push_back(element);
+            return _data.size() - 1;
+        }
+
+    protected:
+        /** @return a pointer to the data array
+         */
+        const value_type* get_data() const
+        {
+            return &_data[0];
+        }
+
+    private:
+        /** Holds all data.
+         */
+        std::vector<value_type> _data;
+};
+
+#endif // __INCLUDED_ABSTRACTARRAY_HPP__
