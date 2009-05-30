638fcfac7ac3c085dfc40f0d711126342c50757d
Giel <Giel@bb1b31de-baa9-489e-af1c-03370f2903b8>
2009-05-30 00:03:55 +0000

    * Make the (cross-) compiler used for Boost configurable (by dynamically rewriting the diff)  * Less hardcoding of Boost values and instead just use Boost's stage as installation area   - Also less hard coding of the Boost version in the associated makefile
    git-svn-id: https://svn.il.fontys.nl/usersvn/fishtank/svn/trunk@296 bb1b31de-baa9-489e-af1c-03370f2903b8

--- fishtank/win32/cross-compile.sh
+++ fishtank/win32/cross-compile.sh
@@ -11,22 +11,8 @@ function execute()
 }
 
 DEVPKG=${HOME}/svn/fishtank/devpkg
-BOOSTDIR="\${top_builddir}/../libs/boost_1_39_0"
-BOOSTLIBDIR=${BOOSTDIR}/include/bin.v2/libs
 
-HOST_TRIPLET=i586-mingw32msvc
-
-COMPILER=gcc-mingw-4.2.1
-BUILD_TYPE=release
-LINK=link-static
-THREADING=threading-multi
-
-BOOST_LIBS="program_options regex thread"
-
-BOOST_LDFLAGS=""
-for lib in $BOOST_LIBS; do
-	BOOST_LDFLAGS="$BOOST_LDFLAGS -L${BOOSTLIBDIR}/${lib}/build/${COMPILER}/${BUILD_TYPE}/${LINK}/target-os-windows/threadapi-win32/${THREADING}"
-done
+HOST_TRIPLET=i686-mingw32
 
 # Make sure we've got build and download directories for our dependencies
 if [ ! -d build ] ; then
@@ -42,6 +28,14 @@ execute make -C libs HOST_TRIPLET=$HOST_TRIPLET
 cd build
 
 execute ../../autogen.sh
-execute ../../configure --host=$HOST_TRIPLET GLFW_CFLAGS="-I\${top_builddir}/../libs/glfw/glfw.bin.WIN32/include" GLFW_LIBS="-L\${top_builddir}/../libs/glfw/glfw.bin.WIN32/lib-mingw -lglfw" --with-boost="${BOOSTDIR}" CPPFLAGS="-I\${top_builddir}/../libs/jpeg/jpeg-6b -I/usr/include/eigen2 -I${DEVPKG}/OpenCV/include" LDFLAGS="-L\${top_builddir}/../libs/jpeg/jpeg-6b ${BOOST_LDFLAGS} -L${DEVPKG}/OpenCV/bin" CFLAGS=-O3 CXXFLAGS=-O3 "$@"
+execute ../../configure --host=$HOST_TRIPLET \
+                        GLFW_CFLAGS="-I`pwd`/../libs/glfw/glfw.bin.WIN32/include" \
+                        GLFW_LIBS="-L`pwd`/../libs/glfw/glfw.bin.WIN32/lib-mingw -lglfw" \
+                        --with-boost="`pwd`/../libs/boost/boost_1_39_0/stage" \
+                        CPPFLAGS="-I`pwd`/../libs/jpeg/jpeg-6b -I/usr/include/eigen2 -I${DEVPKG}/OpenCV/include" \
+                        LDFLAGS="-L`pwd`/../libs/jpeg/jpeg-6b -L${DEVPKG}/OpenCV/bin" \
+                        CFLAGS=-O3 \
+                        CXXFLAGS=-O3 \
+                        "$@"
 execute make
 execute zip -j9 fishtank-win32.zip fishtank.exe ${DEVPKG}/OpenCV/bin/cxcore100.dll ${DEVPKG}/OpenCV/bin/highgui100.dll ${DEVPKG}/OpenCV/bin/libguide40.dll
