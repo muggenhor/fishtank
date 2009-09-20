98bdde4e8f4f77399da61e5e9fbc479b1b611c99
Giel <Giel@bb1b31de-baa9-489e-af1c-03370f2903b8>
2009-09-20 22:58:03 +0000

    Unlike previous revision, this *truly* is the first version of this debug framework
    git-svn-id: https://svn.il.fontys.nl/usersvn/fishtank/svn/trunk@334 bb1b31de-baa9-489e-af1c-03370f2903b8

--- fishtank/lib/framework/debug.cpp
+++ fishtank/lib/framework/debug.cpp
@@ -1,78 +1,15 @@
 #include "debug.hpp"
-#include <boost/foreach.hpp>
-#include <boost/program_options.hpp>
 #include <cassert>
 #include <fstream>
 #include <iostream>
 
-#define foreach BOOST_FOREACH
-
-namespace po = boost::program_options;
-
-boost::array<bool, LOG_LAST> DebugStream::enabled_debug =
-{{
+bool DebugStream::enabled_debug[LOG_LAST] =
+{
 	true, // LOG_ERROR
 #ifndef NDEBUG
 	true, // LOG_WARNING
 #endif
-}};
-
-static void validate(boost::any& v, const std::vector<std::string>& values, code_part*, int)
-{
-	// Make sure no previous assignment to 'a' was made.
-	po::validators::check_first_occurrence(v);
-	// Extract the first string from 'values'. If there is more than
-	// one string, it's an error, and exception will be thrown.
-	const std::string& s = po::validators::get_single_string(values);
-
-	if      (s == "error")
-		v = LOG_ERROR;
-	else if (s == "warning")
-		v = LOG_WARNING;
-	else if (s == "never")
-		v = LOG_NEVER;
-	else if (s == "main")
-		v = LOG_MAIN;
-	else if (s == "media")
-		v = LOG_MEDIA;
-	else if (s == "net")
-		v = LOG_NET;
-	else if (s == "rpc")
-		v = LOG_RPC;
-	else if (s == "memory")
-		v = LOG_MEMORY;
-	else if (s == "gui")
-		v = LOG_GUI;
-	else if (s == "all")
-		v = LOG_ALL;
-	else
-		throw po::validation_error("invalid value");
-}
-
-void DebugStream::addCommandLineOptions(boost::program_options::options_description& desc)
-{
-	desc.add_options()
-		("debug", po::value< std::vector<code_part> >(), "Enable debug messages for given level")
-	;
-}
-
-void DebugStream::processOptions(const boost::program_options::variables_map& options)
-{
-	if (options.count("debug") == 0)
-		return;
-
-	foreach (const code_part& part, options["debug"].as< std::vector<code_part> >())
-	{
-		if (part == LOG_ALL)
-		{
-			foreach (bool& log, enabled_debug)
-				log = true;
-			continue;
-		}
-
-		enabled_debug[part] = true;
-	}
-}
+};
 
 DebugStream::DebugStream(boost::shared_ptr<std::ostream> os) :
 	std::basic_ios<char, std::char_traits<char> >(os->rdbuf()),
@@ -106,7 +43,7 @@ DebugStream::stderr_wrapper::stderr_wrapper() :
 {
 }
 
-DebugStream _debug(const code_part part, const char* const function)
+DebugStream debug(const code_part part)
 {
 	assert(part < LOG_LAST && "debug part out of range");
 
@@ -117,8 +54,6 @@ DebugStream _debug(const code_part part, const char* const function)
 		os.reset(new std::ofstream("/dev/null"));
 
 	DebugStream ds(os);
-
-	ds << "[" << function << "] ";
 
 	switch (part)
 	{
@@ -144,10 +79,6 @@ DebugStream _debug(const code_part part, const char* const function)
 
 		case LOG_NET:
 			ds << "net   : ";
-			break;
-
-		case LOG_RPC:
-			ds << "rpc   : ";
 			break;
 
 		case LOG_MEMORY:
--- fishtank/lib/framework/debug.hpp
+++ fishtank/lib/framework/debug.hpp
@@ -1,15 +1,9 @@
 #ifndef __INCLUDED_LIB_FRAMEWORK_DEBUG_HPP__
 #define __INCLUDED_LIB_FRAMEWORK_DEBUG_HPP__
 
-#include <boost/array.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/thread/mutex.hpp>
 #include <ostream>
-
-namespace boost { namespace program_options {
-class options_description;
-class variables_map;
-}}
 
 enum code_part
 {
@@ -19,22 +13,15 @@ enum code_part
 	LOG_MAIN,
 	LOG_MEDIA,
 	LOG_NET,
-	LOG_RPC,
 	LOG_MEMORY,
 	LOG_GUI,
 
-	LOG_LAST, /**< _must_ be the last valid log level! */
-
-	LOG_ALL, /**< special; used to enable all debug levels */
+	LOG_LAST /**< _must_ be last! */
 };
 
 class DebugStream : public std::ostream
 {
 	public:
-		static void addCommandLineOptions(boost::program_options::options_description& desc);
-
-		static void processOptions(const boost::program_options::variables_map& options);
-
 		DebugStream(const DebugStream& rhs);
 		virtual ~DebugStream();
 
@@ -47,12 +34,12 @@ class DebugStream : public std::ostream
 
 	private:
 		/**
-		 * Construction is only allowed through the @c _debug(code_part, const char*)
+		 * Construction is only allowed through the @c debug(code_part)
 		 * function.
 		 */
 		DebugStream(boost::shared_ptr<std::ostream> os);
 
-		friend DebugStream _debug(code_part, const char*);
+		friend DebugStream debug(code_part);
 
 	private:
 		/** 
@@ -69,18 +56,12 @@ class DebugStream : public std::ostream
 		};
 
 	private:
-		static boost::array<bool, LOG_LAST> enabled_debug;
+		static bool enabled_debug[LOG_LAST];
 
 	private:
 		boost::shared_ptr<std::ostream> _os;
 };
 
-/**
- * Output iostream style str.
- *
- * Only outputs if debugging of part was formerly enabled in DebugStream::enabled_debug.
- */
-#define debug(part) _debug(part, __FUNCTION__)
-DebugStream _debug(code_part part, const char* function);
+DebugStream debug(code_part part);
 
 #endif // __INCLUDED_LIB_FRAMEWORK_DEBUG_HPP__
